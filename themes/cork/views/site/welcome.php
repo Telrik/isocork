2d9777ba790cb7249ed76d93618de00cd3ad7f9c
Telrik <fs@m7.ru>
2014-03-23 14:24:43 +0800

    cl-commit

--- themes/cork/views/site/welcome.php
+++ themes/cork/views/site/welcome.php
@@ -285,138 +285,89 @@
     </div>
 </div>
 
+
 <div class="row">
     <div class="span12">
-        <div class="row">
-            <div class="span12">
-                <div class="aligncenter">
-                    <h3>Наши <strong>Цены</strong></h3>
-
-                    <p>Lorem ipsum dolor sit amet, labores dolorum scriptorem eum an, te quodsi sanctus neglegentur.
-                    </p>
-                </div>
-            </div>
+        <div class="featurette">
+            <h2 class="featurette-heading">ISOCORK <span class="muted"> выпускается нескольких видов:</span></h2>
         </div>
-
-
-        <div class="row">
-
-            <div class="span3">
-                <div class="pricing-box-wrap animated-fast flyIn animated fadeInUp">
-                    <div class="pricing-heading">
-                        <h3>Very <strong>Basic</strong></h3>
-                    </div>
-                    <div class="pricing-terms">
-                        <h6>$15.00 / Month</h6>
-                    </div>
-                    <div class="pricing-content">
-                        <ul>
-                            <li><i class="icon-ok"></i> 100 applications</li>
-                            <li><i class="icon-ok"></i> 24x7 support available</li>
-                            <li><i class="icon-ok"></i> No hidden fees</li>
-                            <li><i class="icon-ok"></i> Free 30-days trial</li>
-                            <li><i class="icon-ok"></i> Stop anytime easily</li>
-                        </ul>
-                    </div>
-                    <div class="pricing-action">
-                        <a class="btn btn-medium btn-theme" href="#"><i class="icon-chevron-down"></i> Sign Up</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="span3">
-                <div class="pricing-box-wrap animated-fast flyIn animated fadeInUp">
-                    <div class="pricing-heading">
-                        <h3>Simple <strong>Choice</strong></h3>
-                    </div>
-                    <div class="pricing-terms">
-                        <h6>$20.00 / Month</h6>
-                    </div>
-                    <div class="pricing-content">
-                        <ul>
-                            <li><i class="icon-ok"></i> 100 applications</li>
-                            <li><i class="icon-ok"></i> 24x7 support available</li>
-                            <li><i class="icon-ok"></i> No hidden fees</li>
-                            <li><i class="icon-ok"></i> Free 30-days trial</li>
-                            <li><i class="icon-ok"></i> Stop anytime easily</li>
-                        </ul>
-                    </div>
-                    <div class="pricing-action">
-                        <a class="btn btn-medium btn-theme" href="#"><i class="icon-chevron-down"></i> Sign Up</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="span3">
-                <div class="pricing-box-wrap special animated-slow flyIn animated fadeInUp">
-                    <div class="pricing-heading">
-                        <h3>Special <strong>Choice</strong></h3>
-                    </div>
-                    <div class="pricing-terms">
-                        <h6>$15.00 / Month</h6>
-                    </div>
-                    <div class="pricing-content">
-                        <ul>
-                            <li><i class="icon-ok"></i> 100 applications</li>
-                            <li><i class="icon-ok"></i> 24x7 support available</li>
-                            <li><i class="icon-ok"></i> No hidden fees</li>
-                            <li><i class="icon-ok"></i> Free 30-days trial</li>
-                            <li><i class="icon-ok"></i> Stop anytime easily</li>
-                        </ul>
-                    </div>
-                    <div class="pricing-action">
-                        <a class="btn btn-medium btn-theme" href="#"><i class="icon-chevron-down"></i> Sign Up</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="span3">
-                <div class="pricing-box-wrap animated flyIn fadeInUp">
-                    <div class="pricing-heading">
-                        <h3>Just <strong>Happy</strong></h3>
-                    </div>
-                    <div class="pricing-terms">
-                        <h6>$15.00 / Month</h6>
-                    </div>
-                    <div class="pricing-content">
-                        <ul>
-                            <li><i class="icon-ok"></i> 100 applications</li>
-                            <li><i class="icon-ok"></i> 24x7 support available</li>
-                            <li><i class="icon-ok"></i> No hidden fees</li>
-                            <li><i class="icon-ok"></i> Free 30-days trial</li>
-                            <li><i class="icon-ok"></i> Stop anytime easily</li>
-                        </ul>
-                    </div>
-                    <div class="pricing-action">
-                        <a class="btn btn-medium btn-theme" href="#"><i class="icon-chevron-down"></i> Sign Up</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </div>
+    <div class="span4">
+        <div class="pricing-box-wrap">
+            <div class="pricing-heading">
+                <h3><strong>УНИВЕРСАЛЬНЫЙ</strong></h3>
+            </div>
+            <div class="pricing-terms">
+                <h6>100 <i class="fa fa-rub"></i> / Ведро</h6>
+            </div>
+            <div class="pricing-content">
+                <ul>
+                    <li><i class="icon-ok"></i> Цвет натуральной пробки</li>
+                </ul>
+            </div>
+            <div class="pricing-action">
+                <a class="btn btn-medium btn-theme" href="#"><i class="fa fa-rub"></i> Купить</a>
+            </div>
+        </div>
+    </div>
+    <div class="span4">
+        <div class="pricing-box-wrap special">
+            <div class="pricing-heading">
+                <h3><strong>ФАСАДНЫЙ</strong></h3>
+            </div>
+            <div class="pricing-terms">
+                <h6>100 <i class="fa fa-rub"></i> / Ведро</h6>
+            </div>
+            <div class="pricing-content">
+                <ul>
+                    <li><i class="icon-ok"></i> 20 стандартных цветов</li>
+                    <li><img width="100%" src="/web/images/vedro3.jpg" alt=""/></li>
 
+                </ul>
+            </div>
+            <div class="pricing-action">
+                <a class="btn btn-medium btn-theme" href="#"><i class="fa fa-rub"></i> Купить</a>
+            </div>
+        </div>
+    </div>
+    <div class="span4">
+        <div class="pricing-box-wrap">
+            <div class="pricing-heading">
+                <h3><strong>ОГНЕУПОРНЫЙ</strong></h3>
+            </div>
+            <div class="pricing-terms">
+                <h6>$35.00 / Month</h6>
+            </div>
 
+            <div class="pricing-content">
+                <ul>
+                    <li><i class="icon-ok"></i> белого цвета</li>
+                </ul>
+            </div>
+            <div class="pricing-action">
+                <a class="btn btn-medium btn-theme" href="#"><i class="fa fa-rub"></i> Купить</a>
+            </div>
+        </div>
+    </div>
 </div>
-
 
 <div class="row">
     <div class="span12 aligncenter">
-        <h3 class="title">What people <strong>saying</strong> about us</h3>
+        <h3 class="title">Люди об <strong>ISOCORK</strong></h3>
 
         <div class="blankline30"></div>
 
         <div class="bx-wrapper" style="max-width: 100%; margin: 0px auto;">
             <div class="bx-viewport" style="width: 100%; overflow: hidden; position: relative; height: 204px;">
-                <ul class="bxslider" style="width: 615%; position: relative; left: -4680px;">
+                <ul class="bxslider" style="width: 615%; position: relative; left: -3510px;">
                     <li style="float: left; list-style: none outside none; position: relative; width: 1170px;" class="bx-clone">
                         <blockquote>
                             Aliquam a orci quis nisi sagittis sagittis. Etiam adipiscing, justo quis feugiat.Suspendisse eu erat quam. Vivamus porttitor eros quis nisi lacinia sed interdum lorem vulputate. Aliquam a orci quis nisi sagittis sagittis.
                             Etiam adipiscing, justo quis feugiat
                         </blockquote>
                         <div class="testimonial-autor">
-                            <img alt="" src="img/dummies/testimonial/4.png">
-                            <h4>Marry Doe Elliot</h4>
+
+                            <h4>Вася пупкин</h4>
                             <a href="#">www.companyname.com</a>
                         </div>
                     </li>
@@ -426,7 +377,6 @@
                             Etiam adipiscing, justo quis feugiat
                         </blockquote>
                         <div class="testimonial-autor">
-                            <img alt="" src="img/dummies/testimonial/1.png?timestamp=1395480608054">
                             <h4>Hillary Doe</h4>
                             <a href="#">www.companyname.com</a>
                         </div>
@@ -437,7 +387,6 @@
                             Etiam adipiscing, justo quis feugiat
                         </blockquote>
                         <div class="testimonial-autor">
-                            <img alt="" src="img/dummies/testimonial/2.png">
                             <h4>Michael Doe</h4>
                             <a href="#">www.companyname.com</a>
                         </div>
@@ -448,7 +397,6 @@
                             Etiam adipiscing, justo quis feugiat
                         </blockquote>
                         <div class="testimonial-autor">
-                            <img alt="" src="img/dummies/testimonial/3.png">
                             <h4>Mark Donovan</h4>
                             <a href="#">www.companyname.com</a>
                         </div>
@@ -459,7 +407,6 @@
                             Etiam adipiscing, justo quis feugiat
                         </blockquote>
                         <div class="testimonial-autor">
-                            <img alt="" src="img/dummies/testimonial/4.png">
                             <h4>Marry Doe Elliot</h4>
                             <a href="#">www.companyname.com</a>
                         </div>
@@ -470,7 +417,6 @@
                             Etiam adipiscing, justo quis feugiat
                         </blockquote>
                         <div class="testimonial-autor">
-                            <img alt="" src="img/dummies/testimonial/1.png?timestamp=1395480608054">
                             <h4>Hillary Doe</h4>
                             <a href="#">www.companyname.com</a>
                         </div>
